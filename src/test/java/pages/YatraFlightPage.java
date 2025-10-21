4e9ad86721db4875ac2ad2c39489549c716cbb78
Lamarnika <Lamarnika1234.gmail.com>
2025-10-21 15:48:42 +0530

    add to new branch

--- src/test/java/pages/YatraFlightPage.java
+++ src/test/java/pages/YatraFlightPage.java
@@ -4,6 +4,7 @@ public class YatraFlightPage {
     public static void main(String[] args) {
         System.out.println("This is new java class for github testing");
         System.out.println("Helle selenium");
+        System.out.println("new branch is created");
     }
 
 }
