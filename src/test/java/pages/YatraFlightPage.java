3eb13ed08f2b6110932b791201193c06f6effcd8
Lamarnika <Lamarnika1234.gmail.com>
2025-10-21 14:44:42 +0530

    added new file to test logout feature

--- src/test/java/pages/YatraFlightPage.java
+++ src/test/java/pages/YatraFlightPage.java
@@ -3,5 +3,7 @@ package pages;
 public class YatraFlightPage {
     public static void main(String[] args) {
         System.out.println("This is new java class for github testing");
+        System.out.println("new branch is created and ready to push on remote rempo");
+        
     }
 }
